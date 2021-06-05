0dc91ccf294887837754401f35a1c80a45b6c42e
victor-singha <vicunite2@gmail.com>
2021-06-05 10:35:00 +0530

    switch

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 const Contact = require("./Contact");
+const prompt = require("prompt-sync")();
 
 let contact = new Contact(
   "john",
@@ -10,5 +11,34 @@ let contact = new Contact(
   6455463372,
   "john.doe@compass.com"
 );
-
-console.log(contact);
+isContinue = true;
+while (isContinue) {
+  console.log(`Enter Choice:
+  Add Contact    [1]
+  Delete Contact [2]
+  Edit Contact   [3]
+  Show Contact   [4]
+  Exit           [5]`);
+  let input = prompt();
+  switch (input) {
+    case "1":
+      console.log(input);
+      break;
+    case "2":
+      console.log(input);
+      break;
+    case "3":
+      console.log(input);
+      break;
+    case "4":
+      console.log(input);
+      break;
+    case "5":
+      console.log(input);
+      isContinue = false;
+      break;
+    default:
+      console.log(input);
+      break;
+  }
+}
